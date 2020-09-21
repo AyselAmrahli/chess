714033ef869980695347e620be4725b8dd6d4e57
ayselamrahli@gmail.com <ayselamrahli@gmail.com>
2020-09-22 00:31:32 +0400

    completed footer

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,3 +1,35 @@
 $(document).ready(function(){
 
+
+
+    // map init function
+function initMap(dataLat,dataLong) {
+    var myLatLng = {lat: dataLat, lng: dataLong};
+    var map = new google.maps.Map(document.getElementById('footer-map'), {
+      center: myLatLng,
+      zoom: 17,
+      disableDefaultUI: true,
+    });
+
+    var contentString = '<div class="d-flex align-items-center"><img src="assets/img/logo-footer.png" /><p>Azerbaycan Sahmat Federasiyasi</p></div>';
+
+
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+      });
+  
+    var image = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABcAAAAhCAMAAAAF18AiAAAAAXNSR0IB2cksfwAAARpQTFRFAAAA1rRp17Rq17Vq17Nq17Rq17Nq2LVq1rVr1LVq1rNq17Rq17Rq17Rq0rRp/39/1rRq17Rq2LRq/6pV17Rp17Rq17Nr17Vs1rRr2LVq3btm17Rr17Nq17Nq17Vr17Np17Rp2LRr1bJr17Rq17Rr1rRq17Vs2bNt17Rq17Rq2LVr2LVr27Zt17Rq2LRr17Rq17Npz69w17Rq17Rq1rRq17Rp2rRq2bRp17Rq1rNn1KpV1rNq17Rq1rhm17Rq17Vq17Rq17Rp27Zt1rZp2LRq1bZo17Rq17Rq17Rq1Kpq1LJm1rRq17Nq1rRq17Rq1LVq17Rq17Nr17Rq17Rq2LVs17Rp17Rq2LVq17Rq07Fv17Rq1rNr17Jp17RrIgD+qAAAAF50Uk5TAESMudjfx6hkGFfU//YRAqrqQQPQUr0tcOIP+c6RhrNmYivL4cNTL/i+T24H84iachD68WOFKSJzJQZe9BnIwOa4DjiVMY67+wweaoDW5DA6lOXgNG3Nr/wX60o/gfI6pNgAAAENSURBVHicbdJnX8IwEAbwxwqKci4ciIoDBQUHoOJW3HsrOPn+XwOau5Sk4d4kz7/pL5e0AFeX1x2J9vTG+mBWf5ykBrzBQIeGyaiRhPDoGNk1zj5B4Ur6POkwpVo8NS1hJj07px/Mt5cv+O9mFjksAVme5aSJKMdlrKgxr3susK+Cx5j2tXWVN8QLwSFTKhfFS5rLmyp72FJjRHuJ120jz5MK846cZhd70n9xHzg4POIQB46D059UT/XU3+7MvR86b/mFy5dqr6zj/MmuwnwtPd/YfHsnfm/7Q3AnjyY/Gf/Js+Evhr+2+c36sdKa32HXh/hnyDPMFYSr5nPdYeCL6Pung+OX/jox/htGaAJjTyQlcU7M0wAAAABJRU5ErkJggg==';
+    var marker = new google.maps.Marker({
+      position: myLatLng,
+      map:map,
+      icon:image
+  });
+
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
+  }
+
+  initMap(40.5899455,49.6676165)
+
 });
